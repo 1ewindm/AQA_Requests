14e890877d5a78cfc80f9a5e5b9bb81acf7be8b0
1ewindm <lewin.dmitry11@gmail.com>
2025-01-12 01:35:52 +0300

    change conftest: scoope = function

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ load_dotenv()
 HOST = "https://dev-gs.qa-playground.com/api/v1" if os.environ ["STAGE"] == "qa" else "https://release-gs.qa-playground.com/api/v1"
 
 
-@pytest.fixture(autouse=True, scope="session")
+@pytest.fixture(autouse=True, scope="function")
 def init_enviroment():
     response = requests.post(
         url=f"{HOST}/setup",
