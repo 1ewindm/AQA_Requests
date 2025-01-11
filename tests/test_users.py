8aa68728540972603e575f5cfe2194a1603b8480
1ewindm <lewin.dmitry11@gmail.com>
2025-01-12 01:37:01 +0300

    test separation: 4 unit test in users_api

--- tests/test_users.py
+++ tests/test_users.py
@@ -6,11 +6,24 @@ load_dotenv()
 class TestUser(BaseTest):
 
     def test_create_users(self):
+        self.api_users.create_user()
+
+    def test_get_all_users(self):
+        self.api_users.get_all_users()
+
+    def test_get_user_by_id(self):
+        # new_user = self.api_users.create_user()
+        all_users = self.api_users.get_all_users()
+        user = all_users.users[0]
+        self.api_users.get_user_by_id(user.uuid)
+
+
+    def test_check_add_new_user_in_list(self):
         new_user = self.api_users.create_user()
         all_users_list = self.api_users.get_all_users()
         assert new_user in all_users_list.users
-        created_user = self.api_users.get_user_by_id(new_user.uuid)
-        print(created_user)
+
+
 
 
 
